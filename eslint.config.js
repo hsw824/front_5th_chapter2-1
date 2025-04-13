6db8a32deeb381238fd61651167761698d9e7f15
hsw824 <ghdtjddn0824@naver.com>
2025-04-13 20:53:48 +0900

    feat: eslint, prettier 추가 및 설정

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { js, prettier: eslintPluginPrettier },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    rules: {
+      'no-unused-vars': 'error',
+      'no-undef': 'error',
+      'no-var': 'error',
+      'no-console': 'error',
+      'prefer-const': 'error',
+
+      'prettier/prettier': 'error',
+      'react/jsx-uses-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
+]);
