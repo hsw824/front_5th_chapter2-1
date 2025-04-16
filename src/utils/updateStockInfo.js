e0892747b0c4984779c5e6db73104b1e0ec012f2
hsw824 <ghdtjddn0824@naver.com>
2025-04-16 22:08:12 +0900

    refactor: 재고 문구 알림 로직 개선

--- src/utils/updateStockInfo.js
+++ src/utils/updateStockInfo.js
@@ -1,14 +1,19 @@
-import { initProdList } from '../initialItems';
+import { globalStore } from './globalStore';
+
+const setStockInfoText = (quantity) => {
+  return quantity > 0 ? `재고 부족 ( ${quantity} 개 남음)` : '품절';
+};
 
 export function updateStockInfo() {
+  const { prodList } = globalStore.getState();
+
   const soldOutInfo = document.getElementById('stock-status');
   if (!soldOutInfo) return;
 
-  var infoMsg = '';
-  initProdList.forEach(function (item) {
-    if (item.quantity < 5) {
-      infoMsg += item.name + ': ' + (item.quantity > 0 ? '재고 부족 (' + item.quantity + '개 남음)' : '품절') + '\n';
-    }
+  let infoMsg = '';
+  prodList.forEach((item) => {
+    if (item.quantity >= 5) return;
+    infoMsg += `${item.name}: ${setStockInfoText(item.quantity)} \n`;
   });
   soldOutInfo.textContent = infoMsg;
 }
