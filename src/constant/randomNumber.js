d52d5fff1129f289b02753c87c64e39f8c73eb30
hsw824 <ghdtjddn0824@naver.com>
2025-04-16 18:00:30 +0900

    feat: 매직넘버 상수화

---
+++ src/constant/randomNumber.js
@@ -0,0 +1,13 @@
+export const TIMER_NUMBER = {
+  FLASH_TIME: Math.random() * 10000,
+  FLASH_INTERVAL: 30000,
+  SUGGEST_TIME: Math.random() * 20000,
+  SUGGEST_INTERVAL: 60000,
+};
+
+export const SALE_NUMBER = {
+  FLASH: 0.8,
+  SUGGEST: 0.95,
+};
+
+export const RANDOM_NUMBER = Math.random() < 0.3;
