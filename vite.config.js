4ed56c384f7f1fcf1a14c424053c9f0c7238e6ae
hsw824 <ghdtjddn0824@naver.com>
2025-04-18 00:58:35 +0900

    fix: ci 오류 제거

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,26 @@
 import react from '@vitejs/plugin-react';
+import path from 'path';
 import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
-
+  base: '/front_5th_chapter2-1/',
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: 'src/setupTests.js',
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        basic: path.resolve(__dirname, 'index.basic.html'),
+        advanced: path.resolve(__dirname, 'index.advanced.html'),
+      },
+    },
   },
 });
