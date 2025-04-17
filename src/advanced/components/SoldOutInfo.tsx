281ceafcf074017f0d9fb69bff0a27183774c1d2
hsw824 <ghdtjddn0824@naver.com>
2025-04-18 00:34:37 +0900

    feat: 품절 정보 로직 및 ui추가

--- src/advanced/components/SoldOutInfo.tsx
+++ src/advanced/components/SoldOutInfo.tsx
@@ -1,5 +1,19 @@
+import { useProdListContext } from '../store/prodList/ProdListContext';
+
+const setStockInfoText = (quantity: number) => {
+  return quantity > 0 ? `재고 부족 ( ${quantity} 개 남음)` : '품절';
+};
+
 const SoldOutInfo = () => {
-  return <div className="text-sm text-gray-500 mt-2">soldOutInfo</div>;
+  const { prodList } = useProdListContext();
+
+  let soldOutInfoMessage = '';
+
+  prodList.forEach((item) => {
+    if (item.quantity >= 5) return;
+    soldOutInfoMessage += `${item.name}: ${setStockInfoText(item.quantity)} \n`;
+  });
+  return <div className="text-sm text-gray-500 mt-2">{soldOutInfoMessage}</div>;
 };
 
 export default SoldOutInfo;
